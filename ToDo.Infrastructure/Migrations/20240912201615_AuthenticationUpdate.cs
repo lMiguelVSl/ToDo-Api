b1f6e05f45edd3634ef4913ad81347ffcdd9e114
Miguel Vargas <miguel.vargas.work@outlook.com>
2024-09-12 15:59:02 -0500

    authentication migrations

---
+++ ToDo.Infrastructure/Migrations/20240912201615_AuthenticationUpdate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDo.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AuthenticationUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authentications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AuthenticationToken = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authentications", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Authentications");
+        }
+    }
+}
